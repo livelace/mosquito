6c1e58513146f4064a6dd4e8718561cde7fb735d
Oleg Popov <o.popov@drweb.com>
2016-09-21 10:35:21 +0300

    Multiple headers supported

--- mosquito/__main__.py
+++ mosquito/__main__.py
@@ -538,7 +538,7 @@ class Mosquito(object):
                                           help='a space separated list of IDs')
         parser_list.set_defaults(func=self.list)        
         
-        results = parser.parse_args('fetch --id 2'.split())
+        results = parser.parse_args()
         results.func(results)
 
 def main():
