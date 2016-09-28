7dea6ad7478817f4144b75ed379658f7ef426de6
Oleg Popov <o.popov@drweb.com>
2016-09-28 08:20:11 +0300

    1

--- mosquito/__main__.py
+++ mosquito/__main__.py
@@ -216,7 +216,7 @@ class Mosquito(object):
         elif grab == 'text':
             try:
                 # Hide HTTP requests 
-                self.logger.setLevel(logging.getLevelName('INFO'))
+                self.logger.setLevel(logging.ERROR)
                 page = requests.get(expanded_url, timeout=float(self.settings.grab_timeout))
                 self.logger.setLevel(self.settings.logger)
                 
